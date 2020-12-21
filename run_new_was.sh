b1dc9983d972c72f94f005b0da714f2d530f5b05
sohwaje <kissenyo@gmail.com>
2020-12-21 14:33:53 +0900

    update

---
+++ run_new_was.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+:<<'END'
+# ervice_url.inc 에서 현재 서비스를 하고 있는 WAS의 포트 번호를 읽어옵니다.
+> cat service_url.inc | grep -Po '[0-9]+'
+127
+0
+0
+1
+8081
+END
+# 현재 사용 중인 Port를 service_url.inc에 가져온다.
+CURRENT_PORT=$(cat /home/azureuser/apps/service_url.inc | grep -Po '[0-9]+' | tail -1)
+# TARGET_PORT는 아래 if문에 따라 결정된다.
+TARGET_PORT=0
+
+echo "> Current port of running WAS is ${CURRENT_PORT}."
+
+# 현재 포트 조건이 참이면 포트를 변경한다.
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081
+else
+  echo "> No WAS is connected to nginx"
+fi
+
+# TARGET_PORT로 TARGET_PID를 찾는다.
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+# TARGET_PID가 빈 문자열이 아니면, TARGET_PID를 죽인다.(만약 타겟포트에도 WAS가 실행중이면 죽인다.)
+if [ ! -z ${TARGET_PID} ]; then
+  echo "> Kill WAS running at ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
+
+# app 실행
+# nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/playground-logging/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+./app-start.sh start
+echo "> Now new WAS runs at ${TARGET_PORT}."
+exit 0
